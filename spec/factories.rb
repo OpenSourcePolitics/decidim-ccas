b808fea0362d8efdadd155135f0a34df79aa430e
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-06-17 13:02:11 +0200

    add missing proposal factorie

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,7 +5,7 @@ require "decidim/core/test/factories"
 # Factories
 
 # require "decidim/participatory_processes/test/factories"
-# require "decidim/proposals/test/factories"
+require "decidim/proposals/test/factories"
 # require "decidim/meetings/test/factories"
 # require "decidim/accountability/test/factories"
 require "decidim/system/test/factories"
